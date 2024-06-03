55be67404e8c4dcf660406724ff83749a4831d0d
fcw <2477376240@qq.com>
2024-06-03 19:58:04 +0800

    优化返回逻辑

--- src/main/java/com/chengwu/onlineJudgecodesandbox/template/JavaCodeSandboxTemplate.java
+++ src/main/java/com/chengwu/onlineJudgecodesandbox/template/JavaCodeSandboxTemplate.java
@@ -130,7 +130,7 @@ public abstract class JavaCodeSandboxTemplate implements CodeSandBox {
             Process compileProcess = Runtime.getRuntime().exec(compileCmd);
             ExecuteMessage executeMessage = ProcessUtils.runProcessAndGetMessage(compileProcess, "编译");
             if (executeMessage.getExitValue() != 0) {
-                throw new RuntimeException("编译错误！");
+                throw new RuntimeException("编译错误");
             }
             return executeMessage;
         } catch (Exception e) {
